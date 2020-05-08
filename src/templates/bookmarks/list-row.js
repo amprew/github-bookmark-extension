8e51df497c6adbd81c7a516e09ab17e327388c31
Andrew Collins <me@andrewww.co.uk>
2020-05-08 23:27:09 +0100

    Remove unrelated github templates attributes.

--- src/templates/bookmarks/list-row.js
+++ src/templates/bookmarks/list-row.js
@@ -12,13 +12,12 @@ export default ({
   const bookmarkPath = `/_bookmark_/${id}`;
 
   return html`
-    <div class="Box-row Box-row--focus-gray p-0 mt-0 js-navigation-item js-issue-row " data-id="604793423">
+    <div class="Box-row Box-row--focus-gray p-0 mt-0">
       <div class="d-flex Box-row--drag-hide position-relative">
         <div class="flex-shrink-0 pt-2 pl-3">
           <svg class="octicon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 16" width="12" height="16"><path fill-rule="evenodd" d="M6 5H2V4h4v1zM2 8h7V7H2v1zm0 2h7V9H2v1zm0 2h7v-1H2v1zm10-7.5V14c0 .55-.45 1-1 1H1c-.55 0-1-.45-1-1V2c0-.55.45-1 1-1h7.5L12 4.5zM11 5L8 2H1v12h10V5z"></path></svg>
         </div>
 
-        <!-- Issue title column -->
         <div class="flex-auto min-width-0 lh-condensed p-2 pr-3 pr-md-2">
           <a class="v-align-middle muted-link h4 pr-1" data-hovercard-type="repository"
             data-hovercard-url="/${org}/${repo}/hovercard"
@@ -56,7 +55,7 @@ export default ({
           </span>
 
           <span class="ml-2 flex-1 flex-shrink-0">
-            <a href="${bookmarkPath}" class="muted-link" aria-label="3 comments">
+            <a href="${bookmarkPath}" class="muted-link">
               <svg class="octicon octicon-comment v-align-middle" viewBox="0 0 16 16" version="1.1" width="16"
                 height="16" aria-hidden="true">
                 <path fill-rule="evenodd"

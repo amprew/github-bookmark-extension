e033e424218893025af6ec1feb7849b6bac399a1
Andrew Collins <me@andrewww.co.uk>
2020-05-16 18:47:53 +0100

    Add link to show page.

--- src/templates/bookmarks/show.js
+++ src/templates/bookmarks/show.js
@@ -56,7 +56,7 @@ export default ({
       <div class="Box mt-3 position-relative">
       <div class="Box-header py-2 d-flex flex-column flex-shrink-0 flex-md-row flex-md-items-center">
         <div class="text-mono f6 flex-auto pr-3 flex-order-2 flex-md-order-1 mt-2 mt-md-0">
-          ${ pathToFile }
+          <a href="${href}">${ pathToFile }</a>
 
           <span class="file-info-divider"></span>
 

8600af2c88f7fc0bc0ad25cc3473677571f636b4
Andrew Collins <me@andrewww.co.uk>
2021-06-20 22:46:00 +0100

    clean bookmark list page style.

--- src/pages/bookmarks/list/index.js
+++ src/pages/bookmarks/list/index.js
@@ -9,14 +9,19 @@ import { getAllBookmarks } from '../../../shared/bookmark';
 /** @jsx h */
 const BookmarkList = ({ bookmarks }) => {
   return (
-    <div className="page-content position-relative container-lg p-responsive width-full">
+    <div className="page-content position-relative container-lg p-responsive width-full pt-5">
       <h2 className="mb-3">Your bookmarks</h2>
+      <hr/>
 
-      <div className="Box Box--responsive hx_Box--firstRowRounded0" data-pjax="">
-        <div data-issue-and-pr-hovercards-enabled="" data-repository-hovercards-enabled="">
-          {bookmarks.map(ListItem)}
+      {bookmarks.length ? (
+        <div className="Box Box--responsive hx_Box--firstRowRounded0" data-pjax="">
+          <div data-issue-and-pr-hovercards-enabled="" data-repository-hovercards-enabled="">
+            {bookmarks.map(ListItem)}
+          </div>
         </div>
-      </div>
+      ) : (
+        <p>No bookmarks currently, please add some and check back!</p>
+      )}
     </div>
   );
 };
